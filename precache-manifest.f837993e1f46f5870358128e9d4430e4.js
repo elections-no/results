3b5e648e9f5553cfe6ce67be782bbe8e89bf5356
Patricia Aas <psmaas@gmail.com>
2019-05-13 15:32:20 +0200

    Updates

---
+++ precache-manifest.f837993e1f46f5870358128e9d4430e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c378367c97aef5069bf52c854590638e",
+    "url": "/results/index.html"
+  },
+  {
+    "revision": "35ac2b01387ac77f9bb7",
+    "url": "/results/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/results/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "35ac2b01387ac77f9bb7",
+    "url": "/results/static/js/main.f82aca33.chunk.js"
+  },
+  {
+    "revision": "9d623f27d936805f2016",
+    "url": "/results/static/js/runtime~main.6639aab3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/results/static/media/logo.5d5d9eef.svg"
+  }
+]);
