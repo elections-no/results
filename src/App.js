79ad4417c45ba2eb068e3af6597779225b8e35e7
Patricia Aas <psmaas@gmail.com>
2019-05-15 09:42:40 +0200

    Try to use the api

--- src/App.js
+++ src/App.js
@@ -2,6 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Feed extends React.Component {
+  state = {
+    election_types: []
+  }
+
+  getElectionTypes() {
+    fetch('https://sleepy-retreat-45150.herokuapp.com/api/election_types')
+      .then(response => {
+        if (!response.ok) {
+          console.log("Response is not ok");
+        }
+        else {
+          console.log("Response is OK!");
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({election_types: data.election_types});
+      });
+  }
+
+  componentDidMount() {
+    this.getElectionTypes();
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.election_types.map(function(item) {
+          return <li key={item.id}>{item.name}</li>;
+        })}
+      </ul>
+    );
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -10,6 +46,7 @@ function App() {
         <p>
           Election Results for 2019
         </p>
+        <Feed/>
       </header>
     </div>
   );
