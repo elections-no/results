9b9279e5ae068bde75e387ba68004aff43dd7653
Patricia Aas <psmaas@gmail.com>
2019-06-06 13:07:46 +0200

    Zoom out on background click

--- src/App.js
+++ src/App.js
@@ -172,8 +172,6 @@ class Norway extends React.Component {
 
     this.handleCountyClick = this.handleCountyClick.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.getBoundingBox = this.getBoundingBox.bind(this);
-    this.getBoundingBoxCenter = this.getBoundingBoxCenter.bind(this);
   }
   projection() {
     const { width, height } = this.props;
@@ -193,33 +191,85 @@ class Norway extends React.Component {
     return "" + x + " " + y + " " + width + " " + height;
   }
 
-  handleCountyClick(countyIndex) {
+  isCountySelected(countyIndex) {
     const key = "#" + this.makeId("county", countyIndex);
     const selection = d3.select(key);
-    const was_selected = selection.classed("active");
+    return selection.classed("active");
+  }
 
+  getCounty(countyIndex) {
+    const key = "#" + this.makeId("county", countyIndex);
+    return d3.select(key);
+  }
+
+  selectCounty(selection) {
+    selection.classed("active", true);
+  }
+
+  handleCountyClick(countyIndex) {
+    this.setClickHandled();
+
+    const was_selected = this.isCountySelected(countyIndex);
+    this.deselectAllCounties();
+
+    if (!was_selected) {
+      const selection = this.getCounty(countyIndex);
+      this.selectCounty(selection);
+      const { x, y, width, height } = this.getBoundingBox(selection);
+      this.zoomIn(this.makeViewBoxString(x, y, width, height));
+    } else {
+      this.zoomOut();
+    }
+
+    console.log("Clicked on county: ", this.getCountyName(countyIndex));
+    console.log("Clicked on county number: ", this.getCountyNumber(countyIndex));
+  }
+
+  zoomIn(view) {
+    d3.select("svg").attr("viewBox", view);
+  }
+
+  zoomOut() {
+    const { width, height } = this.props;
+    let view = this.makeViewBoxString(0, 0, width, height);
+    d3.select("svg").attr("viewBox", view);
+  }
+
+  getCountyNumber(countyIndex) {
+    return this.state.counties[countyIndex].properties.ID_1;
+  }
+
+  getCountyName(countyIndex) {
+    return this.state.counties[countyIndex].properties.NAME_1;
+  }
+
+  setClickHandled() {
+    d3.select("#" + this.props.id).classed("clicked", true);
+  }
+
+  resetClickHandled() {
+    d3.select("#" + this.props.id).classed("clicked", false);
+  }
+
+  isClickHandled() {
+    return d3.select("#" + this.props.id).classed("clicked");
+  }
+
+  deselectAllCounties() {
     d3.selectAll("path").classed("active", (d, i, nodes) => {
       const node = d3.select(nodes[i]);
       node.classed("active", false);
     });
-
-    if (!was_selected) {
-      selection.classed("active", true);
-      const { x, y, width, height } = this.getBoundingBox(selection);
-      let view = this.makeViewBoxString(x, y, width, height);
-      d3.select("svg").attr("viewBox", view);
-    } else {
-      const { width, height } = this.props;
-      let view = this.makeViewBoxString(0, 0, width, height);
-      d3.select("svg").attr("viewBox", view);
-    }
-
-    console.log("Clicked on county: ", this.state.counties[countyIndex].properties.NAME_1);
-    console.log("Clicked on county number: ", this.state.counties[countyIndex].properties.ID_1);
   }
 
   handleClick() {
-    console.log("Clicked!");
+    if (this.isClickHandled()) {
+      this.resetClickHandled();
+    } else {
+      console.log("Clicked!");
+      this.deselectAllCounties();
+      this.zoomOut();
+    }
   }
 
   componentDidMount() {
